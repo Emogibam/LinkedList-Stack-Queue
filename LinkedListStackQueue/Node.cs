8d6834e72306b40a5c81a257f0d2c8cbc5bb0768
Ogidan Emmanuel <eogidan22@gmail.com>
2022-05-18 07:50:54 +0100

    My first commit

---
+++ LinkedListStackQueue/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListStackQueue
+{
+    class Node<T>
+    {
+        public T Data;
+        public Node<T> Next;
+        public Node(T input)
+        {
+
+            Data = input;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# LinkedList-Stack-Queue" 
